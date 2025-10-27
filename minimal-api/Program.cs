c5019411b9a148a70cc5ec3527336316b0203ad8
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-27 18:36:46 -0300

    feat: Get para retornar todos os veículos
    Organiza o código em #region
    Organiza rotas por contexto no Swagger

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -10,31 +10,40 @@ using MinimalApi.DTOs;
 using MinimalApi.Infraestrutura.Db;
 #endregion
 
-#region Builder e Configurações
+#region Configuração da Aplicação
+
 var builder = WebApplication.CreateBuilder(args);
 
-#region Serviços
+#region Configuração de Serviços
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minha API (.NET 9)", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Minha API (.NET 9)",
+        Version = "v1"
+    });
 });
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
-builder.Services.AddDbContext<DbContexto>(options => {
+builder.Services.AddDbContext<DbContexto>(options =>
+{
     options.UseMySql(
         builder.Configuration.GetConnectionString("mysql"),
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql")));
 });
 #endregion
+
 #endregion
 
-#region App e Middlewares
+#region Configuração do Pipeline
+
 var app = builder.Build();
 
-#region Swagger
+#region Middleware Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -46,35 +55,51 @@ if (app.Environment.IsDevelopment())
 #endregion
 
 app.UseHttpsRedirection();
+
 #endregion
 
-#region Home
-app.MapGet("/", () => Results.Json(new Home()));
+#region Rotas
+
+#region Rota Inicial
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
 #endregion
 
-#region Administradores
-app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
+#region Rotas de Administradores
+app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+{
     if (administradorServico.Login(loginDTO) != null)
         return Results.Ok("Login com sucesso");
     else
         return Results.Unauthorized();
-});
+}).WithTags("Administradores");
 #endregion
 
-#region Veiculos
-app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => {
+#region Rotas de Veículos
+
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
     var veiculo = new Veiculo
     {
         Nome = veiculoDTO.Nome,
         Marca = veiculoDTO.Marca,
         Ano = veiculoDTO.Ano
     };
+
     veiculoServico.Incluir(veiculo);
 
     return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
-}); 
+}).WithTags("Veículos");
+
+app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) =>
+{
+    var veiculos = veiculoServico.Todos(pagina);
+    return Results.Ok(veiculos);
+}).WithTags("Veículos");
+
 #endregion
 
-#region Execução
+#endregion
+
+#region Execução da Aplicação
 app.Run();
 #endregion
