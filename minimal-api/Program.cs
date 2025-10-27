7c006eb9928aa3aa8fa19bc7c87744d66cacd9d6
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-27 16:46:40 -0300

    feat: POST para criar veículo

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,14 +1,19 @@
+#region Usings
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using minimal_api.Dominio.Interfaces;
 using minimal_api.Dominio.ModelViews;
 using minimal_api.Dominio.Servicos;
+using MinimalApi.Dominio.Entidades;
 using MinimalApi.DTOs;
 using MinimalApi.Infraestrutura.Db;
+#endregion
 
+#region Builder e Configurações
 var builder = WebApplication.CreateBuilder(args);
 
+#region Serviços
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -16,15 +21,20 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddDbContext<DbContexto>(options => {
     options.UseMySql(
         builder.Configuration.GetConnectionString("mysql"),
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql")));
 });
+#endregion
+#endregion
 
+#region App e Middlewares
 var app = builder.Build();
 
+#region Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -33,16 +43,38 @@ if (app.Environment.IsDevelopment())
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
     });
 }
-
-app.MapGet("/", () => Results.Json(new Home()));
-
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
-     if (administradorServico.Login(loginDTO) != null)
-         return Results.Ok("Login com sucesso");
-     else
-         return Results.Unauthorized();
-});
+#endregion
 
 app.UseHttpsRedirection();
+#endregion
 
+#region Home
+app.MapGet("/", () => Results.Json(new Home()));
+#endregion
+
+#region Administradores
+app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
+    if (administradorServico.Login(loginDTO) != null)
+        return Results.Ok("Login com sucesso");
+    else
+        return Results.Unauthorized();
+});
+#endregion
+
+#region Veiculos
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => {
+    var veiculo = new Veiculo
+    {
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano
+    };
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+}); 
+#endregion
+
+#region Execução
 app.Run();
+#endregion
